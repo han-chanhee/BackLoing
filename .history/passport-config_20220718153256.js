271ec431dcad793a88642ef852815293b94300a7
chlwpdns <shebal1563@naver.com>
2022-08-09 20:49:53 +0900

    first commit

---
+++ .history/passport-config_20220718153256.js
@@ -0,0 +1,36 @@
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcrypt');
+
+function initialize(passport, getUserByEmail) {
+  // 이메일,패스워드, 인증이왈료될떄 done
+  const authenticateUser = async (email, password, done) => {
+    // 사용자 이메일 받기
+    const user = getUserByEmail(email);
+    // 사용자가이메일이없으면
+    if (user == null) {
+      // 첫번쨰 매개변수 서버 오류 , 사용자 못찾을떄
+      return done(null, false, { message: 'No user with that email' });
+    }
+    // 패스워드 오류  구현
+    try {
+      // 사용자가입력한 암호와 비교 사용자가있을때
+      if (await bcrypt.compare(password, user.password)) {
+        return done(null, user);
+      } else {
+        // 비번일치 안할때
+        return done(null, false, { message: 'Password incorrect' });
+      }
+    } catch (e) {
+      return done(e);
+    }
+  };
+
+  passport.use(new LocalStrategy({ usernameField: 'email' }, authenticateUser));
+
+  passport.serializeUser((user, done) => done(null, user.id));
+  passport.deserializeUser((id, done) => {
+    return done(null, getUserById(id));
+  });
+}
+
+module.exports = initialize;
---
+++ .history/server_20220809183654.js
@@ -0,0 +1,99 @@
+// 환경변수 로드 만약 개발환경이
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+
+const express = require('express');
+const app = express();
+// 비밀번호를 해쉬화
+const bcrypt = require('bcrypt');
+const passport = require('passport');
+const flash = require('express-flash');
+const session = require('express-session');
+const methodOverride = require('method-override');
+
+const initializePassport = require('./passport-config');
+
+// 초기 passport
+initializePassport(
+  passport,
+  (email) => users.find((user) => user.email === email),
+  (id) => users.find((user) => user.id === id)
+);
+// express-session
+// express-flash
+
+// !주의 사용하면안되는데 이렇게 데이터베이스를 하지만 인증 애플리케이션 위주이기 때무에 전체 데이터 베이스에 연결 하면 좆됨진짜
+
+//  https://www.youtube.com/watch?v=XlvsJLer_No&list=PLZlA0Gpn_vH8jbFkBjOuFjhxANC63OmXM   : 링크 보면서 공부
+//   https://www.youtube.com/watch?v=Ud5xKCYQTjM
+
+const users = [];
+
+// ejs 구문을 사용하고 있다고 알려줌 실제 템플릿에서 사용이 가능하다
+app.set('view-engine', 'ejs');
+// url 로된 인코딩을 확장옵션으로 전달받고싶음
+// dotenv와 사용세션과 함께 작동할 여권 세션을 사용
+app.use(express.urlencoded({ extended: false }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    //세션 빈값 저장
+    saveUninitialized: false,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req, res) => {
+  // 특정 요청 페이지
+  res.render('index.ejs', { name: 'chlwpdns' });
+});
+
+app.get('/login', (req, res) => {
+  // 특정 요청 페이지
+
+  res.render('login.ejs');
+});
+
+app.post(
+  '/login',
+  // 실패 성공 인증 라우터
+  passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+  })
+);
+
+//////////
+
+app.get('/register', (req, res) => {
+  // 특정 요청 페이지
+  res.render('register.ejs');
+});
+
+app.post('/register', async (req, res) => {
+  //해시함수 비동기 암호화 사용
+
+  try {
+    // 10은 가장좋은 기본적인 해시함수 10으로 설정
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    // 새로운 사용자 등록
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword,
+    });
+    res.redirect('/login');
+  } catch {
+    res.redirect('/register');
+  }
+  // name 필드에 해당하는 것으로 접속 변수내부 접속입니다
+  console.log(users);
+});
+
+app.listen(3000);
